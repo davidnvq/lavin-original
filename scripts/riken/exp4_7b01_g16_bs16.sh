ad4e767a4f99ae9e2f8637eb56d0b9d2c16a835f
Nguyen Van Quang <quanguet@gmail.com>
2023-11-04 21:55:28 +0900

    add scripts exp3-5

--- scripts/riken/exp4_7b01_g16_bs16.sh
+++ scripts/riken/exp4_7b01_g16_bs16.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#$-S /bin/bash
+#$-cwd
+#$-ac d=none
+#$-j y
+#$-o $HOME/log/$JOB_ID
+#$ -N exp4_7b01_g16_bs16
+#$-jc gs-container_g16.24h
+
+# logging $HOME/log/$JOB_ID
+
 # For internet connection
 export MY_PROXY_URL="http://10.1.10.1:8080/"
 export HTTP_PROXY=$MY_PROXY_URL
@@ -10,36 +21,36 @@ export ftp_proxy=$MY_PROXY_URL
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate lavin
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-torchrun --nproc_per_node 4 --master_port 12141 train.py \
+export EXPNAME="exp4_7b01_g16_bs16"
+
+torchrun --nproc_per_node 16 --master_port 11141 train.py \
     --wandb_enable \
     --llm_model 7B \
     --llama_model_path ./data/weights/ \
     --data_path ./data/captions.json \
     --max_seq_len 512 \
-    --batch_size 4 \
-    --accum_iter 2 \
+    --batch_size 1 \
+    --accum_iter 1 \
     --epochs 20 \
     --warmup_epochs 2 \
     --blr 9e-3 \
     --weight_decay 0.02 \
-    --output_dir ./outputs/exp3_7b01_gt4_bs32 \
-    --adapter_type attn \
-    --adapter_dim 8 \
-    --adapter_scale 1 \
+    --output_dir ./outputs/${EXPNAME} \
+    --adapter_type attn\
+    --adapter_dim 8\
+    --adapter_scale 1\
     --n_prompt 6 \
     --prompt_format QCM-ALE \
     --temperature 10.\
     --visual_adapter_type router
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-torchrun --nproc_per_node 1 --master_port 12311 eval.py \
+torchrun --nproc_per_node 1 --master_port 11311 eval.py \
     --ckpt_dir ./data/weights/ \
     --llm_model 7B \
     --tokenizer_path ./data/weights/tokenizer.model \
     --data_root ./data \
     --caption_file ./data/captions.json \
-    --adapter_path ./outputs/exp3_7b01_gt4_bs32/checkpoint-19.pth \
+    --adapter_path ./outputs/${EXPNAME}/checkpoint-19.pth \
     --adapter_type attn \
     --adapter_dim 8 \
     --adapter_scale 1 \
--- scripts/riken/exp5_7b01_gt4_bs16_2.sh
+++ scripts/riken/exp5_7b01_gt4_bs16_2.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#$-S /bin/bash
+#$-cwd
+#$-ac d=none
+#$-j y
+#$-o $HOME/log/$JOB_ID
+#$ -N exp5_7b01_gt4_bs16_2
+#$-jc gtn-container_g4.24h
+
+# logging $HOME/log/$JOB_ID
+
 # For internet connection
 export MY_PROXY_URL="http://10.1.10.1:8080/"
 export HTTP_PROXY=$MY_PROXY_URL
@@ -10,36 +21,36 @@ export ftp_proxy=$MY_PROXY_URL
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate lavin
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-torchrun --nproc_per_node 4 --master_port 12141 train.py \
+export EXPNAME="exp5_7b01_gt4_bs16_2"
+
+torchrun --nproc_per_node 4 --master_port 11141 train.py \
     --wandb_enable \
     --llm_model 7B \
     --llama_model_path ./data/weights/ \
     --data_path ./data/captions.json \
     --max_seq_len 512 \
-    --batch_size 4 \
+    --batch_size 2 \
     --accum_iter 2 \
     --epochs 20 \
     --warmup_epochs 2 \
     --blr 9e-3 \
     --weight_decay 0.02 \
-    --output_dir ./outputs/exp3_7b01_gt4_bs32 \
-    --adapter_type attn \
-    --adapter_dim 8 \
-    --adapter_scale 1 \
+    --output_dir ./outputs/${EXPNAME} \
+    --adapter_type attn\
+    --adapter_dim 8\
+    --adapter_scale 1\
     --n_prompt 6 \
     --prompt_format QCM-ALE \
     --temperature 10.\
     --visual_adapter_type router
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-torchrun --nproc_per_node 1 --master_port 12311 eval.py \
+torchrun --nproc_per_node 1 --master_port 11311 eval.py \
     --ckpt_dir ./data/weights/ \
     --llm_model 7B \
     --tokenizer_path ./data/weights/tokenizer.model \
     --data_root ./data \
     --caption_file ./data/captions.json \
-    --adapter_path ./outputs/exp3_7b01_gt4_bs32/checkpoint-19.pth \
+    --adapter_path ./outputs/${EXPNAME}/checkpoint-19.pth \
     --adapter_type attn \
     --adapter_dim 8 \
     --adapter_scale 1 \
