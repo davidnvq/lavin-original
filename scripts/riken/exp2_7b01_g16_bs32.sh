d33efbe94402d84ad76c411eceb0d57aa94417d2
Nguyen Van Quang <quanguet@gmail.com>
2023-11-04 21:22:04 +0900

    update exp2 script

--- scripts/riken/exp2_7b01_g16_bs32.sh
+++ scripts/riken/exp2_7b01_g16_bs32.sh
@@ -1,3 +1,12 @@
+# For internet connection
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
+
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate lavin
 
