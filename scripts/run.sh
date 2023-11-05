7e1609085878097b2d90c92abc5f693019bc8596
Nguyen Van Quang <quanguet@gmail.com>
2023-11-05 10:55:29 +0900

    restructure script folders

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,9 @@
-qsub scripts/riken/exp7_7b02-chat_g16_bs_32-torch1.1.sh
-qsub scripts/riken/exp7_7b02-chat_g16_bs_32-torch2.1.sh
-qsub scripts/riken/exp7_7b02_g16_bs_32-torch1.1.sh
-qsub scripts/riken/exp7_7b02_g16_bs_32-torch2.1.sh
-qsub scripts/riken/exp8_7b02-chat_gt4_bs16-4_torch1.1.sh
-qsub scripts/riken/exp8_7b02-chat_gt4_bs16-4_torch2.1.sh
-qsub scripts/riken/exp8_7b02_gt4_bs16-4_torch1.1.sh
-qsub scripts/riken/exp8_7b02_gt4_bs16-4_torch2.1.sh
+qsub scripts/riken/gtn/exp8_7b02-chat_gt4_bs16-4_torch1.1.sh
+qsub scripts/riken/gtn/exp8_7b02-chat_gt4_bs16-4_torch2.1.sh
+qsub scripts/riken/gtn/exp8_7b02_gt4_bs16-4_torch1.1.sh
+qsub scripts/riken/gtn/exp8_7b02_gt4_bs16-4_torch2.1.sh
+
+qsub scripts/riken/g16/exp7_7b02-chat_g16_bs_32-torch1.1.sh
+qsub scripts/riken/g16/exp7_7b02-chat_g16_bs_32-torch2.1.sh
+qsub scripts/riken/g16/exp7_7b02_g16_bs_32-torch1.1.sh
+qsub scripts/riken/g16/exp7_7b02_g16_bs_32-torch2.1.sh
