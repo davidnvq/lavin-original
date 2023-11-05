8a6bd889936550a060228ca7a20b71308e386650
Nguyen Van Quang <quanguet@gmail.com>
2023-11-06 08:46:32 +0900

    add eval scripts

---
+++ scripts/dhpr/eval/exp1_var_g16_bs32.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$-S /bin/bash
+#$-cwd
+#$-ac d=none
+#$-j y
+#$-o $HOME/log/eval_exp1_var_g16_bs32
+#$ -N "eval_exp1_var_g16_bs32"
+#$-jc gtb-container_g1.24h
+
+
+# For internet connection
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate lavin-torch2.1
+
+export CKPT="checkpoint-19.pth"
+export EXPNAME="exp1_var_g16_bs32"
+
+torchrun --nproc_per_node 1 --master_port 16813 eval_dhpr.py \
+    --adapter_path ./outputs/${EXPNAME}/${CKPT} \
+    --batch_size 4
+
+
