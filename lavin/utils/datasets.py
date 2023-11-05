3cbeb4bad9493c01be51830d672b70608115a50c
Nguyen Van Quang <quanguet@gmail.com>
2023-11-05 20:13:00 +0900

    add DHPR dataset

--- lavin/utils/datasets.py
+++ lavin/utils/datasets.py
@@ -12,16 +12,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json, re, random
-import torch.utils.data as Data
-from torchvision.transforms import transforms
 import os
-from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
-from PIL import Image
-from lavin.utils.base_prompt import *
-import torch
-from lavin import Tokenizer
 import copy
+import json, re, random
+import os.path as osp
+import torch
+import torch.utils.data as Data
+
+from PIL import Image
+from PIL import Image, ImageDraw
+from torchvision.transforms import transforms
+from torchvision.transforms.functional import InterpolationMode
+import torch.utils.data as Data
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, RandomCrop, ColorJitter
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+from lavin import Tokenizer
+from lavin.utils.base_prompt import *
 
 
 class ScienceQADataSet(Data.Dataset):
@@ -100,36 +107,60 @@ class ScienceQADataSet(Data.Dataset):
         random.shuffle(list)
 
 
-class InstrcutDataSet(Data.Dataset):
+PROMPTS = {
+    'A':
+        'Based on my dashcam image, what is the potential hazard? Response:',
+    'B':
+        'what is the potential hazard? Response:',
+    'C':
+        'You are a highly accurate decision-making reasoning assistant. \
+        Provide reasoning hazard based on the image marked up with color boxes. \
+        It is essential to distinguish between the box colors and answer questions related to them.',
+}
 
-    def __init__(self, args, split, model_path, max_words=512, max_image_feats=1):
-        super(InstrcutDataSet, self).__init__()
-        self.args = args
-        # --------------------------
-        # ---- Raw data loading ---
-        # --------------------------
-        self.data = json.load(open(os.path.join(args.data_root, 'all_data.json')))[split]
 
-        self.tokenizer = Tokenizer(model_path=model_path + '/tokenizer.model')
-        self.max_words = max_words
-        self.max_image_feats = max_image_feats
+class DHPRDataset:
+
+    def __init__(self,
+                 split='train',
+                 root='/home/quang/workspace/lavin-original/data',
+                 max_words=128,
+                 box_type='highlight',
+                 n_pix=224,
+                 prompt_id='A',
+                 has_speed=False):
         self.split = split
-        self.qids = [item['qid'] for item in self.data]
+        self.root_path = root
+        self.max_words = max_words
+        self.tokenizer = Tokenizer(model_path=osp.join(root, 'weights/tokenizer.model'))
+        self.anno_data = json.load(open(osp.join(root, f'dhpr_annotations/v1101_meta_anno_{split}.json')))
+        self.image_ids = list(self.anno_data.keys())
 
-        print(f"number of problems in split {split}: {len(self.qids)}\n")
+        print(f"Split {split}: {len(self.image_ids)} items!")
 
-        self.transforms = transforms.Compose([
-            transforms.Resize((224, 224), interpolation=Image.BICUBIC),
-            transforms.ToTensor(),
+        self.prompt = PROMPTS[prompt_id]
+        self.has_speed = has_speed
+
+        self.box_type = box_type
+        self.transforms = self._transform(n_pix)
+        self.colorjitter = ColorJitter(brightness=.5, hue=.3)
+
+    def _transform(self, n_px):
+        resize_px = n_px + 16 if self.split == 'train' else n_px
+        SplitCrop = RandomCrop if self.split == 'train' else CenterCrop
+        return Compose([
+            Resize((resize_px, resize_px), interpolation=InterpolationMode.BICUBIC),
+            SplitCrop(n_px),
+            ToTensor(),
             transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
         ])
 
-    def tokenize(self, prompt, answer, max_words=512):
+    def tokenize(self, prompt, answer):
         example = prompt + answer
         # print(prompt)
         prompt = torch.tensor(self.tokenizer.encode(prompt, bos=True, eos=False), dtype=torch.int64)
         example = torch.tensor(self.tokenizer.encode(example, bos=True, eos=True), dtype=torch.int64)
-        padding = max_words - example.shape[0]
+        padding = self.max_words - example.shape[0]
         if padding > 0:
             example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
         elif padding < 0:
@@ -144,60 +175,60 @@ class InstrcutDataSet(Data.Dataset):
         label_mask = label_mask.float()
         return example, labels, example_mask, label_mask
 
-    def __getitem__(self, idx):
+    def hide_region(self, image, bboxes):
+        COLOR_FILL = ['#ff05cd3c', '#00F1E83c', '#F2D4003c']  # 3 color - pink, green, yellow
 
-        prompt_question = self.data[idx]['instruction']
-        prompt_answer = self.data[idx]['answer']
+        if self.split == 'train':
+            image = self.colorjitter(image)
 
-        if self.data[idx]['image'] is not None:
-            # image_path='../data/images/train' if self.data[idx]['image_source']=='sqa' else '../data/images/train2014'
-            if self.data[idx]['image_source'] == 'sqa':
-                image = Image.open(os.path.join('../data/images/train', self.qids[idx], 'image.png')).convert('RGB')
-            else:
-                image = Image.open(os.path.join('../data/images/train2014', 'COCO_train2014_' + self.data[idx]['image'])).convert('RGB')
+        image = image.convert('RGBA')
+        overlay = Image.new('RGBA', image.size, '#00000000')
+        draw = ImageDraw.Draw(overlay, 'RGBA')
+
+        for idx, bbox in enumerate(bboxes):
+            x1, y1, x2, y2 = bbox
+            color_fill = COLOR_FILL[idx]
+            draw.rectangle([(x1, y1), (x2, y2)], fill=color_fill, outline='#05ff37ff', width=5)
+        image = Image.alpha_composite(image, overlay)
+        return image.convert('RGB')
+
+    def get_item(self, image_id):
+        item = self.anno_data[image_id]
+        image_name = f'{image_id}.jpg' if '-' in image_id else f'{image_id}.png'
+
+        image = Image.open(os.path.join(self.root_path, 'dhpr_images', image_name)).convert('RGB')
+        image = self.hide_region(image, item['bounding_box'])
+
+        answer = item['hazard']
+        return image.convert("RGB"), answer
+
+    def __getitem__(self, idx, do_transform=True):
+        image_id = self.image_ids[idx]
+        item = self.anno_data[image_id]
+        image_name = f'{image_id}.jpg' if '-' in image_id else f'{image_id}.png'
+        image = Image.open(os.path.join(self.root_path, 'dhpr_images', image_name)).convert('RGB')
+        image = self.hide_region(image, item['bounding_box'])
+
+        if do_transform:
             image = self.transforms(image)
-            indicator = 1
-        else:
-            image = torch.Tensor(torch.zeros(3, 224, 224).float())
-            indicator = 0
+        indicator = 1
 
-        # print(prompt_question,prompt_answer)
-        example, labels, example_mask, label_mask = self.tokenize(prompt_question, prompt_answer)
+        answer = item['hazard']
 
+        prompt = self.prompt
+        if self.has_speed:
+            prompt = f'Our car has the plausible speed of {item["plausible_speed"]}.' + prompt
+        if self.split != 'train':
+            return image, indicator, prompt, answer, image_id
+
+        example, labels, example_mask, label_mask = self.tokenize(prompt, answer)
         return example, labels, example_mask, image, indicator
 
     def __len__(self):
-        return len(self.qids)
-
-    def shuffle_list(self, list):
-        random.shuffle(list)
+        return len(self.anno_data)
 
 
 if __name__ == '__main__':
-    from torch.utils.data import DataLoader
-
-    class Cfg():
-
-        def __init__(self):
-            super(Cfg, self).__init__()
-            self.options = ["A", "B", "C", "D", "E"]
-            self.use_caption = True
-            self.prompt_format = 'CQM-A'
-            self.data_root = './data'
-            self.output_root = './output'
-            self.caption_file = './data/captions.json'
-
-    cfg = Cfg()
-    dataset = ScienceQADataSet(cfg, 'val', './data/weights')
-    data_loader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
-    max_question_len = 0
-    max_answer_len = 0
-    #406 max question
-    for prompt_questions, question_mask, images, image_masks, prompt_answers, answers, qids in data_loader:
-        print(prompt_questions)
-        print(answers)
-    #     if len(prompt_questions[0].split())>max_question_len:
-    #         max_question_len=len(prompt_questions[0].split())
-    #     if len(prompt_answers[0].split())>max_answer_len:
-    #         max_answer_len=len(prompt_answers[0].split())
-    # print(max_question_len,max_answer_len)
+    dataset = DHPRDataset(split='train', box_type='highlight')
+    example, labels, example_mask, image, indicator = dataset[0]
+    print("OK")
