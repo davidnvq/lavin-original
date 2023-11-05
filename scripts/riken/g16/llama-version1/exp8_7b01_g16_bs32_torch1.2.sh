d1fffc87bfe477d39d995a19992576ebd283afa7
Nguyen Van Quang <quanguet@gmail.com>
2023-11-05 17:46:22 +0900

    scripts to compare torch versions

---
+++ scripts/riken/g16/llama-version1/exp8_7b01_g16_bs32_torch1.2.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#$-S /bin/bash
+#$-cwd
+#$-ac d=none
+#$-j y
+#$-o $HOME/log/$JOB_ID
+#$ -N "exp8_7b01_g16_bs32_torch1.2"
+#$-jc gs-container_g16.24h
+
+
+# For internet connection
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate lavin
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+export EXPNAME="exp8_7b01_g16_bs32_torch1.2"
+
+torchrun --nproc_per_node 16 --master_port 12341 train.py \
+    --wandb_enable \
+    --llm_model 7B \
+    --llama_model_path ./data/weights/ \
+    --data_path ./data/captions.json \
+    --max_seq_len 512 \
+    --batch_size 2 \
+    --accum_iter 1 \
+    --epochs 20 \
+    --warmup_epochs 2 \
+    --blr 9e-3 \
+    --weight_decay 0.02 \
+    --output_dir ./outputs/${EXPNAME} \
+    --adapter_type attn\
+    --adapter_dim 8\
+    --adapter_scale 1\
+    --n_prompt 6 \
+    --prompt_format QCM-ALE \
+    --temperature 10.\
+    --visual_adapter_type router
+
+export ckpt=${EXPNAME}/checkpoint-19.pth
+torchrun --nproc_per_node 1 --master_port 12342 eval.py \
+    --ckpt_dir ./data/weights/ \
+    --llm_model 7B \
+    --tokenizer_path ./data/weights/tokenizer.model \
+    --data_root ./data \
+    --caption_file ./data/captions.json \
+    --adapter_path ./outputs/${ckpt} \
+    --adapter_type attn \
+    --adapter_dim 8 \
+    --adapter_scale 1 \
+    --prompt_format QCM-ALE \
+    --max_batch_size 64 \
+    --max_seq_len 256 \
+    --split test \
+    --n_prompt 6 \
+    --temperature 10.\
+    --visual_adapter_type router \
+    --generation_temperature 0.0 \
+    --wandb_name ${ckpt}
